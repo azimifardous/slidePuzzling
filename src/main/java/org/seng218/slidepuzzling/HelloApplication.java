6cc006f7d5541de9ad47344c7fc204d2f0f23d77
abdi <magus.abdi@gmail.com>
2024-03-12 20:09:53 +0300

    - s

--- src/main/java/org/seng218/slidepuzzling/HelloApplication.java
+++ src/main/java/org/seng218/slidepuzzling/HelloApplication.java
@@ -14,6 +14,8 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        System.out.println("2");
+        //printed-----------------_______
         stage.show();
     }
 
